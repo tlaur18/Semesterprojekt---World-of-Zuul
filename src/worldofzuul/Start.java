d12f34a9497fa6db9f89b540237b785860200af4
gokartn <43138809+gokartn@users.noreply.github.com>
2018-10-09 10:46:11 +0200

    World of Zuul

---
+++ src/worldofzuul/Start.java
@@ -0,0 +1,17 @@
+
+package worldofzuul;
+/**
+ *
+ * @author Morten K. Jensen
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Game game = new Game();
+     
+     game.play();
+    }    
+}
