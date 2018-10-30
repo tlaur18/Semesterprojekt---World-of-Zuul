e59a85eb6caa17cbae37b4998393a2334b04fdcb
ngPsych <43138865+ngPsych@users.noreply.github.com>
2018-10-30 13:12:50 +0100

    Health feature
    lostHealth fixed

--- src/worldofzuul/Player.java
+++ src/worldofzuul/Player.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package worldofzuul;
 
-/**
- *
- * @author Morten K. Jensen
- */
 public class Player {
 
     private int stepCount = 0;
@@ -40,12 +32,12 @@ public class Player {
     }
 
     public int looseHealth() {
-        health -= 25;
+        health -= lostHealth;
         return health;
     }
 
     public int lostHealth() {
-        return this.lostHealth;
+        return lostHealth;
     }
     
     public boolean isDead() {
