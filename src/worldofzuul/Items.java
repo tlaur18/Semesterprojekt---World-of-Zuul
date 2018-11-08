aea768b4d34771992c4a76bc5605e2e9b1cfa7e6
tlaur18 <43138775+tlaur18@users.noreply.github.com>
2018-11-08 17:21:35 +0100

    Fjernet grimme kommentarer

--- src/worldofzuul/Items.java
+++ src/worldofzuul/Items.java
@@ -10,30 +10,18 @@ public class Items {
         this.description = description;
     }
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the description
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description the description to set
-     */
     public void setDescription(String description) {
         this.description = description;
     }
