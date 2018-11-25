969bcfb8984ddeeaaf56cab31cf0e8d3c950710d
tlaur18 <43138775+tlaur18@users.noreply.github.com>
2018-11-25 10:55:25 +0100

    Exteder application nu

--- src/worldofzuul/Start.java
+++ src/worldofzuul/Start.java
@@ -1,8 +1,25 @@
 package worldofzuul;
 
 import worldofzuulIO.TextIO;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class Start {
+public class Start extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     public static void main(String[] args) throws InterruptedException {
         TextIO textIO = new TextIO(new Game());
